3ca249df1b9e0c54d378c08b1f318a8e21f12f59
rafalkkk <mobilo@wp.pl>
2024-01-06 20:32:14 +0100

    merge

--- app.py
+++ app.py
@@ -7,8 +7,9 @@ from flask import Flask, url_for, request, redirect
 app = Flask(__name__)
 
 def dyn():
+    """Function generates dynamic route"""
     return f'Dynamically added route<br><a href={ url_for("hello_world") }>take me home</a>'
-    app.add_url_rule('/dynamic', 'dynamic_route', dyn)
+app.add_url_rule('/dynamic', 'dynamic_route', dyn)
 
 # The route() function of the Flask class is a decorator
 # which tells the application which URL should call
@@ -16,6 +17,7 @@ def dyn():
 @app.route('/')
 # ‘/’ URL is bound with hello_world() function.
 def hello_world():
+    """Function generates hello_world page"""
     content = f'''<html>
       <body>     
       <form action = { url_for("login") } method = "post">
@@ -31,19 +33,20 @@ def hello_world():
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+    """Function shows login page"""
     if request.method == 'POST':
         user = request.form['nm']
         return redirect(url_for('success', name=user))
-    else:
-        user = request.args.get('nm')
-        return redirect(url_for('success', name=user))
+    user = request.args.get('nm')
+    return redirect(url_for('success', name=user))
 
 @app.route('/success/<name>')
 def success(name):
+    """Function generates the success message"""
     return f'welcome {name} <br><a href={ url_for("hello_world") }>take me home</a>'
 
 # main driver function
 if __name__ == '__main__':
 	# run() method of Flask class runs the application
 	# on the local development server.
-	app.run()
+    app.run()
